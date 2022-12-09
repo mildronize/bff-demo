dcbd88c1dee9174958c7e654fa6cde905cd1a97c
Thada Wangthammang <thada.wth@gmail.com>
2022-12-09 16:20:30 +0700

    feat: setup vite for multi-page app

--- apps/web-app/vite.config.ts
+++ apps/web-app/vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  root,
+  server: {
+    port: 4200,
+  },
+  plugins: [react()],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        about: resolve(root, 'about', 'index.html'),
+      }
+    }
+  }
 })
