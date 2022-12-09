c9f5a9a28a4a5258f653ce70e816e92ffcce7260
Thada Wangthammang <thada.wth@gmail.com>
2022-12-09 10:09:15 +0700

    feat: add react web-app with vite

---
+++ apps/web-app/src/app/app.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <h2>Hello</h2>
+    </StyledApp>
+  );
+}
+
+export default App;
